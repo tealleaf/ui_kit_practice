57eb34560972194cc3d741612caf56d4b9316ca9
Quynh <bequynh09@gmail.com>
2018-05-08 21:48:17 -0400

    Index change.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
+    <title>UI Kit Fun</title>
+</head>
+
+<body>
+	
+	<!-- NAV BAR https://www.swiss.com/worldofswiss/en/ -->
+	<nav class="uk-navbar-container" uk-navbar>
+	    <div class="uk-navbar-left">
+	        <ul class="uk-navbar-nav">
+	            <li class="uk-active"><a href="">Logo</a></li>
+	            <li class="uk-parent"><a href="">Book</a></li>
+	            <li><a href="">Prepare</a></li>
+	            <li><a href="">Fly</a></li>
+	        </ul>
+	    </div>
+	</nav>
+	
+	<!-- ======== SCRIPTS ======== -->
+	<script src="js/uikit.min.js"></script>
+	<script src="js/uikit-icons.min.js"></script>	
+</body>
+</html>
